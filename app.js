1bf62565653c9411176c06df643fc61df80144bf
peter.chen <peter.chen@104.com.tw>
2019-01-14 18:34:13 +0800

    ajax post method with json data

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('view engine','ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 
 // get method
@@ -20,10 +27,27 @@ app.get('/action_page',function(req,res) {
   res.render('action_page',values);
 });
 
+app.get('/httpRequest/get',function(req,res) {
+  res.send('Hi! Peter, This\'s time to start!');
+});
+
+// post method
+app.post('/httpRequest/post', function (req, res) {
+  console.log("後端：")
+  console.log(req.body);
+
+  var jsonData = {
+    name:req.body.name+"123",
+    city:req.body.city+"456"
+  }
+  // res.send('Got a POST request');
+  // res.send(req.body);
+  res.send(jsonData);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
 
 
 // my function
